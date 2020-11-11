d872453e46a0ce534ff3c1552916fdcd6136821b
Francklin Medeiros Rodrigues <FMRodrigues@rd.com.br>
2020-11-11 08:56:56 -0300

    card modificado

--- js/script.js
+++ js/script.js
@@ -3,15 +3,16 @@ let vlInput = document.querySelector('#vlInput');
 let perfil = document.querySelector('#perfil');
 let body = document.querySelector('body');
 
-body.addEventListener('keyup', e => {
+
+window.onkeyup = (e) =>{
     if (vlInput.value === 'USUARIO NÃO ENCONTRADO') {
         vlInput.value = '';
         vlInput.style.background = "white";
     }
     if (e.keyCode === 13) {
-        return consomeAPI();
+        return ajaxApi('https://api.github.com/users/' + vlInput.value);
     }
-})
+}
 
 vlInput.addEventListener('focus', () => {
     if (vlInput.value === 'USUARIO NÃO ENCONTRADO') {
@@ -21,33 +22,24 @@ vlInput.addEventListener('focus', () => {
 })
 
 buscar.addEventListener('click', () => {
-    return consomeAPI();
+    ajaxApi('https://api.github.com/users/' + vlInput.value)
+
 })
 
-const consomeAPI = () => {
-    let config = {
-        method: "GET"
-    }
 
-    if (vlInput.value === '' || vlInput.value === null) {
-        alert("Insira um nome para buscar")
-    } else {
+const ajaxApi = (urlChamada) => {
+    let config = { method: "GET" }
 
-        let resposta = fetch('https://api.github.com/users/' + vlInput.value, config)
-            .then((resposta) => {
-                return resposta.json();
-            }).then((dados) => {
-                if (dados.message === "Not Found") {
-                    vlInput.value = '';
-                    vlInput.value = "USUARIO NÃO ENCONTRADO";
-                    vlInput.style.background = "red";
-
-                } else {
-                    montaCard(dados);
-                }
-            })
-    }
+    fetch(urlChamada, config)
+        .then((resposta) => {
+            if (resposta.ok) {
+                return resposta.json()
+            }
+        })
+        .then(dados => montaCard(dados))
+        .catch(() => tratamentoERRO())
 }
+
 
 const montaCard = (dados) => {
 
@@ -57,24 +49,37 @@ const montaCard = (dados) => {
     let criado = ((dataC.getDate()) + "/" + ((dataC.getMonth() + 1)) + "/" + dataC.getFullYear());
 
     perfil.innerHTML = `
-<div id=card>
-<div id="imgP"><img src="${dados.avatar_url}" alt="avatar" width="300px"  title="${dados.name}"></div>
-<ul id="dados">
-<li>Login: <span>${dados.login === null ? 'Dados não encontrados' : dados.login}</span></li>
-<li>Nome: <span>${dados.name === null ? 'Dados não encontrados' : dados.name}</span></li>
-<li>Localização: <span>${dados.location === null ? 'Dados não encontrados' : dados.location}</span></li>
-<li>Twitter: <span>${dados.twitter_username === null ? 'Dados não encontrados' : dados.twitter_username}</span></li>
-<li>Blog: <span>${dados.blog === null ? 'Dados não encontrados' : dados.blog}</span></li>
-<li>Email: <span>${dados.email === null ? 'Dados não encontrados' : dados.email}</span></li>
-<li>Bio: <span>${dados.bio === null ? 'Dados não encontrados' : dados.bio}</span></li>
-<li>Companhia: <span>${dados.company === null ? 'Dados não encontrados' : dados.company}</span></li>
-<li>Repositórios: <span>${dados.public_repos === null ? 'Dados não encontrados' : dados.public_repos}</span></li>
-<li>Seguidores: <span>${dados.followers === null ? 'Dados não encontrados' : dados.followers}</span></li>
-<li>Seguindo: <span>${dados.following === null ? 'Dados não encontrados' : dados.following}</span></li>
-<li>Criado: <span>${criado === null ? 'Dados não encontrados' : criado}</span></li>
-<li>Última atualização: <span>${ultAtualizacao === null ? 'Dados não encontrados' : ultAtualizacao}</span></li>
-<li>URL: <span><a href="${dados.html_url === null ? 'Dados não encontrados' : dados.html_url}" target="_blank" >${dados.html_url === null ? 'Dados não encontrados' : dados.html_url}</a></span> </li>
-</ul>
-</div>
+    <a href="${dados.html_url === null ? 'Dados não encontrados' : dados.html_url}" target="_blank">
+    <div id=card>
+        <img src="${dados.avatar_url}" alt="avatar"  title="${dados.name}">
+        <ul id="dadosSimples">
+        <li>Login: <span>${dados.login === null ? 'Dados não encontrados' : dados.login}</span></li>
+            <li>Nome: <span>${dados.name === null ? 'Dados não encontrados' : dados.name}</span></li>
+            <li>Localização: <span>${dados.location === null ? 'Dados não encontrados' : dados.location}</span></li>
+            <li>Twitter: <span>${dados.twitter_username === null ? 'Dados não encontrados' :
+                    dados.twitter_username}</span></li>
+            <li>Blog: <span>${dados.blog === null ? 'Dados não encontrados' : dados.blog}</span></li>
+            <li>Email: <span>${dados.email === null ? 'Dados não encontrados' : dados.email}</span></li>
+            <li>Seguidores: <span>${dados.followers === null ? 'Dados não encontrados' : dados.followers}</span></li>
+            <li>Seguindo: <span>${dados.following === null ? 'Dados não encontrados' : dados.following}</span></li>
+            <li>Criado: <span>${criado === null ? 'Dados não encontrados' : criado}</span></li>
+            <li>Última atualização: <span>${ultAtualizacao === null ? 'Dados não encontrados' : ultAtualizacao}</span>
+            </li>
+            <li>Companhia: <span>${dados.company === null ? 'Dados não encontrados' : dados.company}</span></li>
+            <li>Repositórios: <span>${dados.public_repos === null ? 'Dados não encontrados' : dados.public_repos}</span>
+            </li>
+            <li>URL: <span>${dados.html_url === null ? 'Dados não encontrados' : dados.html_url}</span> </li>
+        </ul>
+        <ul id="dados">
+            <li>Bio: <span>${dados.bio === null ? 'Dados não encontrados' : dados.bio}</span></li>
+        </ul>
+        </div>
+        </a>
 `
+}
+
+const tratamentoERRO = () => {
+    vlInput.value = '';
+    vlInput.value = 'USUARIO NÃO ENCONTRADO';
+    vlInput.style.background = 'red';
 }
